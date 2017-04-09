39ed1d9ff5da51539dfcccce1f52a1c89fbaf892
Anthony Cerruti <aprime2003@gmail.com>
2017-04-09 16:02:28 -0700

    hello

---
+++ src/Mason.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class Mason {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null, "wake and then take a shower");
+	JOptionPane.showMessageDialog(null, "put clothes on");
+	JOptionPane.showMessageDialog(null, "do hair and brush teeth");
+	JOptionPane.showMessageDialog(null, "eat eggs and bacon");
+	JOptionPane.showMessageDialog(null, "put shoes on");
+	JOptionPane.showMessageDialog(null, "go on phone");
+}
+}
