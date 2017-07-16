67b7d2b2e3ce824fdff9b0e5049337ecf9896c9c
Leagueathyst <league@WTS5.local>
2017-07-16 16:05:38 -0700

    keth is awesome

---
+++ src/yearsalives.java
@@ -0,0 +1,29 @@
+import java.util.Iterator;
+
+import javax.swing.JOptionPane;
+
+public class yearsalives {
+public static void main(String[] args) {
+	String ans= JOptionPane.showInputDialog("what is your age");
+	int age= Integer.parseInt(ans);
+	for (int i = 0; i <= age; i++) {
+		System.out.println(i);
+	} 
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
